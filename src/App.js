bda06789fb340119219256e1b6537079c1dd2fe5
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-04-21 21:20:41 -0300

    Codigo Clase 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Clases-react-1121
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
