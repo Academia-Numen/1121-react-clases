baa70f7d90e5c55b6114f3796a0b4ea8e67ee9a9
Aaron Ivan Amaro <aaronivanamaro@gmail.com>
2022-05-12 20:38:43 -0300

    Sincronizamos efecto con users (json placeholder api)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function ListItem({ id, contenido, deleteItem }) {
@@ -19,17 +20,53 @@ function App() {
     { id: 4, value: 'Comer sano' },
   ]);
 
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState({
+    message: '',
+    isError: false,
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
   function eliminarItem(identificador) {
     const listaFiltrada = items.filter(item => item.id !== identificador);
     setItems(listaFiltrada);
   }
 
+  useEffect(() => {
+    setIsLoading(true);
+    axios('https://jsonplaceholder.typicode.com/user')
+      .then(res => setUsers(res.data))
+      .catch(err => setError({
+        message: err.message,
+        isError: true,
+      }))
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1500);
+      });
+  }, [items]);
+
   return (
-    <ul>
-      {items.map((item) => (
-        <ListItem key={item.id} contenido={item.value} id={item.id} deleteItem={eliminarItem} />
-      ))}
-    </ul>
+    <>
+      <ul>
+        {items.map((item) => (
+          <ListItem key={item.id} contenido={item.value} id={item.id} deleteItem={eliminarItem} />
+        ))}
+      </ul>
+
+      {
+        error.isError ? <h4>{error.message}</h4>
+
+          : isLoading ? <h4>Cargando...</h4> :
+
+            <ul>
+              {users.map(user => (
+                <li key={user.id}>{user.name}</li>
+              ))}
+            </ul>
+      }
+    </>
   );
 }
 
